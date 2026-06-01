31491bc6c6372085f04d375c0b649c2a9293a14c
Nicholas Tran <nicholas.tran2006@gmail.com>
2026-06-01 16:08:16 -0500

    Update version

--- src/main/java/megawalls/MegaWallsMod.java
+++ src/main/java/megawalls/MegaWallsMod.java
@@ -23,7 +23,7 @@ public final class MegaWallsMod {
 
     public static final String MOD_ID = "qol";
     public static final String MOD_NAME = "qol";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.2.0";
 
     public static MegaWallsConfig config;
     private static File sourceFile;
